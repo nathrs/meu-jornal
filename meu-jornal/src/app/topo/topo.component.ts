d083049f49cdd8188c4015e5be5a1c11af0b97a0
Student <ribeirosnathalia@gmail.com>
2018-04-18 12:00:09 -0300

    Criação de rodapé

---
+++ meu-jornal/src/app/topo/topo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector:'app-topo',                  // declara de onde vem o componente - nome da pasta criada (topo)
+    templateUrl:'./topo.component.html' // diz de onde vem
+})
+export class TopoComponent {
+    
+}
