309da3412d4bbb781919a4fa2ddbb61db6944c97
Student <ribeirosnathalia@gmail.com>
2018-04-19 17:17:57 -0300

    modulos

--- meu-jornal/src/app/app.module.ts
+++ meu-jornal/src/app/app.module.ts
@@ -1,31 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { NoticiaComponent } from './noticia/noticia.component';
-import { AlertaComponent } from './alerta/alerta.component';
-import { PrimeiroExemploComponent } from './primeiro-exemplo/primeiro-exemplo.component';
-import { SegundoExemploComponent } from './segundo-exemplo/segundo-exemplo.component';
-import { TerceiroExemploComponent } from './terceiro-exemplo/terceiro-exemplo.component';
-
+import { NoticiaModule } from './noticia/noticia.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
     RodapeComponent,
-    NoticiaComponent,
-    AlertaComponent,
-    PrimeiroExemploComponent,
-    SegundoExemploComponent,
-    TerceiroExemploComponent
+    
   ],
   imports: [
     BrowserModule, 
-    FormsModule
+    NoticiaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
