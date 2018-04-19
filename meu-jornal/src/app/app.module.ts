0eb922508dec5f29894a546fff221148dbfe0352
Student <ribeirosnathalia@gmail.com>
2018-04-19 17:24:17 -0300

    modulos

--- meu-jornal/src/app/app.module.ts
+++ meu-jornal/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NoticiaModule } from './noticia/noticia.module';
+
 
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { RodapeComponent } from './rodape/rodape.component';
-import { NoticiaComponent } from './noticia/noticia.component';
-import { NoticiaModule } from './noticia/noticia.module';
+
 
 @NgModule({
   declarations: [
